4d72b0dcc6e5780fbc56d92ef719894dc3c6f8cc
Sajjad Anwar <sajjadkm@gmail.com>
2019-08-23 19:44:02 +0530

    fix fetching params, remove invalid attributes

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+read -p 'Stack name: ' stackname
+read -sp 'DB password: ' dbpassword
+
 aws cloudformation deploy --template-file cloudformation.yml --stack-name \
-ohm-nominatim --tags Project=ohm-nominatim --region us-east-1 --capabilities CAPABILITY_IAM
+$stackname --tags Project=ohm-nominatim --region us-east-1 --capabilities CAPABILITY_IAM \
+--parameter-overrides DBPassword=$dbpassword
