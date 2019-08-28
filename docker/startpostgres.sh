6cb8698101da5321e8b9bf89736e1c90c681342d
Sanjay B <sanjaybhangar@gmail.com>
2019-08-28 16:34:51 +0530

    get postgres running and api service able to reach db server (need to still run init.sh)

--- docker/startpostgres.sh
+++ docker/startpostgres.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 PGDIR=$1
 
-sudo mkdir -p /data/$PGDIR && export PGDATA=/data/$PGDIR
-sudo chown -R postgres:postgres /data/$PGDIR 
+# sudo mkdir -p /data/$PGDIR && export PGDATA=/data/$PGDIR
+# sudo chown -R postgres:postgres /data/$PGDIR 
 
-sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /data/$PGDIR && \
-sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR start && \
+# sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /data/$PGDIR && \
+# sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR start && \
+sudo service postgresql start && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
+sudo -u postgres psql postgres -c "CREATE DATABASE nominatim" && \
 
-# service postgresql start
+sudo service postgresql restart && \
 tail -f /var/log/postgresql/postgresql-11-main.log
