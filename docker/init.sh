e67679ac4f0cbca663c9ed9479a1122613402b69
Sajjad Anwar <sajjadkm@gmail.com>
2019-08-28 17:31:12 +0530

    import only if db doesn't exist

--- docker/init.sh
+++ docker/init.sh
@@ -2,11 +2,18 @@ OSMFILEURL=$1
 PGDIR=$2
 THREADS=$3
 
-mkdir -p /data/$PGDIR && \
-wget $1 -O osmfile.osm.bz2
-OSMFILE=osmfile.osm.bz2
-
-# export  PGDATA=/data/$PGDIR  && \
 useradd -m -p password1234 nominatim && \
-chown -R nominatim:nominatim ./src && \
-./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS 
+chown -R nominatim:nominatim ./src
+
+if psql -lqt | cut -d \| -f 1 | grep -qw 'nominatim'; then
+    echo 'DB exists. Skip importing'
+    exit 0
+else
+    echo 'DB does not exist. Importing'
+    mkdir -p /data/$PGDIR && \
+    wget $1 -O osmfile.osm.bz2
+    OSMFILE=osmfile.osm.bz2
+
+    # export  PGDATA=/data/$PGDIR  && \
+    ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS
+fi
--- docker/startpostgres.sh
+++ docker/startpostgres.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 PGDIR=$1
 
-# sudo mkdir -p /data/$PGDIR && export PGDATA=/data/$PGDIR
-# sudo chown -R postgres:postgres /data/$PGDIR 
-
-# sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /data/$PGDIR && \
-# sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR start && \
 sudo service postgresql start && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
