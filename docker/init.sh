ea78c258ba1854c5ad2e048cedc7d9d85404c511
Sajjad Anwar <sajjadkm@gmail.com>
2019-08-29 13:25:45 +0530

    create users, supply stackname

--- docker/init.sh
+++ docker/init.sh
@@ -5,11 +5,31 @@ THREADS=$3
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src
 
-if psql -lqt | cut -d \| -f 1 | grep -qw 'nominatim'; then
-    echo 'DB exists. Skip importing'
-    exit 0
+export PGPASSWORD=$PG_PASSWORD
+if psql -h $PG_HOST -p $PG_PORT -U $PG_USER -lqt | cut -d \| -f 1 | grep -qw 'nominatim'; then
+    if psql -h $PG_HOST -p $PG_PORT -U $PG_USER -tAc "SELECT 1 FROM import_status" | grep -q 1; then
+        echo 'DB exists. Skip importing'
+        exit 0
+    else
+        echo 'Tables does not exist, setup db'
+        psql -h $PG_HOST -p $PG_PORT -U $PG_USER -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || createuser -h $PG_HOST -p $PG_PORT -U $PG_USER -s nominatim && \
+        psql -h $PG_HOST -p $PG_PORT -U $PG_USER -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || createuser -h $PG_HOST -p $PG_PORT -U $PG_USER -SDR www-data && \
+        mkdir -p /data/$PGDIR && \
+        wget $1 -q -O osmfile.osm.bz2
+        OSMFILE=osmfile.osm.bz2
+
+        # export  PGDATA=/data/$PGDIR  && \
+        ./src/build/utils/setup.php --osm-file $OSMFILE --setup-db \
+         --import-data --create-functions \
+         --create-tables --create-partition-tables \
+         --create-partition-functions --import-wikipedia-articles \
+         --load-data --import-tiger-data \
+         --calculate-postcodes --index \
+         --create-search-indices --create-country-names \
+         --threads $THREADS
+    fi
 else
-    echo 'DB does not exist. Importing'
+    echo 'DB does not exist. Run all operations'
     mkdir -p /data/$PGDIR && \
     wget $1 -q -O osmfile.osm.bz2
     OSMFILE=osmfile.osm.bz2
